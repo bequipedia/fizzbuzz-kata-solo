0a9956adead07b6e2db25895467792026e6822e5
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-08 15:31:22 +0200

    refactor to return the input as a string

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,12 +1,5 @@
 public class FizzBuzz {
     public String convert(int inputNumber) {
-        String result = "1";
-        if(inputNumber == 3) {
-            result = "3";
-        }
-        if(inputNumber == 2) {
-            result = "2";
-        }
-        return result;
+        return String.valueOf(inputNumber);
     }
 }
