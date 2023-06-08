2e16fb0ad8a37884fc7f02494a72088d13a940e6
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-08 16:08:37 +0200

    refactor to remove duplicated code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -6,11 +6,12 @@ public class FizzBuzz {
     public String convert(int inputNumber) {
         String result = String.valueOf(inputNumber);
         boolean isMultipleOf3 = inputNumber % 3 == 0;
+        boolean isMultipleOf5 = inputNumber % 5 == 0;
 
         if(isMultipleOf3) {
             result = FIZZ;
         }
-        if(inputNumber == 5 || inputNumber == 10 || inputNumber == 20) {
+        if(isMultipleOf5) {
             result = BUZZ;
         }
 
