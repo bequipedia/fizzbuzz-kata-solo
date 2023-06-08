30e6c2cee16a3f9b169cd8895d66a972509999e2
Alasdair Smith <alasdairsmith0491@gmail.com>
2023-06-08 14:58:41 +0100

    refactor to improve readability and comprehensibility

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,14 +1,13 @@
 public class FizzBuzz {
+
+    public static final String FIZZ = "Fizz";
+
     public String convert(int inputNumber) {
         String result = String.valueOf(inputNumber);
-        if(inputNumber == 3) {
-            result = "Fizz";
-        }
-        if(inputNumber == 6) {
-            result = "Fizz";
-        }
-        if(inputNumber == 9) {
-            result = "Fizz";
+        boolean isMultipleOf3 = inputNumber % 3 == 0;
+
+        if(isMultipleOf3) {
+            result = FIZZ;
         }
         return result;
     }
