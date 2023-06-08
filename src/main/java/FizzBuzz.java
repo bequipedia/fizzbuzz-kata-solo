4cd4fc4d7c4a842ebc512c70d590772abe410e6f
Rebeca Gonzalez <bequipedia@gmail.com>
2023-06-08 16:16:36 +0200

    refactor to remove duplicated code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,11 +2,13 @@ public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final String BUZZ = "Buzz";
+    public static final String FIZZ_BUZZ = "FizzBuzz";
 
     public String convert(int inputNumber) {
         String result = String.valueOf(inputNumber);
         boolean isMultipleOf3 = inputNumber % 3 == 0;
         boolean isMultipleOf5 = inputNumber % 5 == 0;
+        boolean isMultipleOf3and5 = inputNumber % 15 == 0;
 
         if(isMultipleOf3) {
             result = FIZZ;
@@ -14,14 +16,8 @@ public class FizzBuzz {
         if(isMultipleOf5) {
             result = BUZZ;
         }
-        if(inputNumber == 15) {
-            return "FizzBuzz";
-        }
-        if(inputNumber == 30) {
-            return "FizzBuzz";
-        }
-        if(inputNumber == 45) {
-            return "FizzBuzz";
+        if(isMultipleOf3and5) {
+            result = FIZZ_BUZZ;
         }
 
         return result;
